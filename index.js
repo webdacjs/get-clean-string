adee7ee81969b676835abb04123a9d05a064c87a
webdacjs <me@jco.im>
2019-08-14 23:06:31 +0200

    Dealing with no string passed to the function

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const charFile = require('./charactersMap.json')
 const charMap = new Map(charFile)
 const aRange = [31, 123]
 
-const between = (v, min, max) => v > min && v < max
+const betweenFn = (v, min, max) => v > min && v < max
 
 function assignToMap (custom) {
   Object.keys(custom).filter(x => x.length === 1).forEach(k => {
@@ -14,10 +14,10 @@ module.exports = function (str, sep = ' ', custom) {
   if (custom && typeof (custom) === 'object') {
     assignToMap(custom)
   }
-  return str
+  return (str || '')
     .toLowerCase()
     .split('')
     .map(s => charMap.has(s) ? charMap.get(s) : s)
-    .map(s => between(s.charCodeAt(), aRange[0], aRange[1]) ? s : '')
+    .map(s => betweenFn(s.charCodeAt(0), aRange[0], aRange[1]) ? s : '')
     .join('').trim().replace(/\s{1,}/g, sep)
 }
