0b7915df61cc09423334fa37039326fa89aa5ab6
webdacjs <me@jco.im>
2019-08-15 06:48:30 +0200

    Supporting global overrides

--- index.js
+++ index.js
@@ -5,19 +5,26 @@ const aRange = [31, 123]
 const betweenFn = (v, min, max) => v > min && v < max
 
 function assignToMap (custom) {
-  Object.keys(custom).filter(x => x.length === 1).forEach(k => {
-    charMap.set(k, custom[k])
-  })
+  if (custom && typeof (custom) === 'object') {
+    Object.keys(custom).filter(x => x.length === 1).forEach(k => {
+      charMap.set(k, custom[k])
+    })
+  }
 }
 
-module.exports = function (str, sep = ' ', custom) {
-  if (custom && typeof (custom) === 'object') {
+function getReplacament (charater) {
+  const rep = charMap.has(charater) ? charMap.get(charater) : charater
+  return betweenFn(rep.charCodeAt(0), aRange[0], aRange[1]) ? rep : ''
+}
+
+module.exports = function (sep = ' ', custom) {
+  const defaultSeparator = sep
+  assignToMap(custom)
+  return function (str = '', sep = defaultSeparator, custom) {
     assignToMap(custom)
+    return String(str).toLowerCase()
+      .split('')
+      .map(s => getReplacament(s))
+      .join('').trim().replace(/\s{1,}/g, sep)
   }
-  return (str || '')
-    .toLowerCase()
-    .split('')
-    .map(s => charMap.has(s) ? charMap.get(s) : s)
-    .map(s => betweenFn(s.charCodeAt(0), aRange[0], aRange[1]) ? s : '')
-    .join('').trim().replace(/\s{1,}/g, sep)
 }
